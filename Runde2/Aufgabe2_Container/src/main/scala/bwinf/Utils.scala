124421a06b1feef79211d51626c6701da1264960
Tim Dev <tim8dev@gmail.com>
2011-03-24 22:02:07 +0100

    reshaping Utils.scala

--- Runde2/Aufgabe2_Container/src/main/scala/bwinf/Utils.scala
+++ Runde2/Aufgabe2_Container/src/main/scala/bwinf/Utils.scala
@@ -15,31 +15,30 @@ object Utils {
       a(di) = a(i-1)
       a(i-1) = swap
     }
-    a map (_ + 1) toList // Drop first, because don't need the heading 0.
+    a map (_ + 1) // from 0..n -> 1...n
   }
 
-  /* Must return a List of Cycles sorted by _.head */
-  def cyclesOf(perm: Seq[Int]) = cycles(Nil, perm) //filterNot (_.tail.isEmpty)
+  /** Return the list of disjunct cycles sorted by cycle.head */
+  def cyclesOf(perm: Seq[Int]) = cycles(Nil, perm)
   
   @tailrec
   private def cycles(ready: Cycles, perm: Seq[Int], handled: Set[Int] = Set(), start: Int = 1): Cycles = // c * 
     if(start > perm.length || start < 1) ready.reverse
     else {
-      val trans = cycle(perm, start) // O(n)
-      val newHandled = handled ++ trans
+      val aCycle = cycle(perm, start) // O(n)
+      val newHandled = handled ++ aCycle
       val next = (start to perm.length) findIndexOf (i => !(newHandled contains i)) // O(n * log n)
       if(next >= 0)
-        cycles(trans :: ready, perm, newHandled, next+start)
+        cycles(aCycle :: ready, perm, newHandled, next+start)
       else
-        (trans :: ready).reverse // O(n)
+        (aCycle :: ready).reverse // O(n)
     }
-  private def cycle(perm: Seq[Int], start: Int) = { // O(n)
-    val startIdx = start // - 1 // from 1...n -> 0..n
-    val transPerm = perm //map (_ - 1)
-    (startIdx :: helper(Nil, transPerm, startIdx)(transPerm(startIdx - 1))) // map (_ + 1) // from 0..n -> 1...n
-  }
+  /** Small helper function, finding one cycle. */
+  private def cycle(perm: Seq[Int], start: Int) = // O(n_c)
+    (start :: helper(Nil, perm, start)(perm(start - 1)))
+
   @tailrec
-  private def helper(ready: List[Int], perm: Seq[Int], start: Int)(idx: Int): Cycle = // | O(n)
+  private def helper(ready: List[Int], perm: Seq[Int], start: Int)(idx: Int): Cycle = // O(n_c)
     if(start == idx) ready.reverse // Reverse it! | O(n)
     else helper(idx :: ready, perm, start)(perm(idx - 1))
 }
