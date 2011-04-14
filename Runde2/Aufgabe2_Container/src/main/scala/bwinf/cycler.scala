5b240ea7ae84af179c09eeb3bf4907c580f499c0
Tim Dev <tim8dev@gmail.com>
2011-04-14 07:34:13 +0200

    Aufgabe2.tex really almost ready (98%)

--- Runde2/Aufgabe2_Container/src/main/scala/bwinf/cycler.scala
+++ Runde2/Aufgabe2_Container/src/main/scala/bwinf/cycler.scala
@@ -24,11 +24,11 @@ object SlowCycler extends Cycler {
     start :: step(perm(start-1))
   }
   def cyclesOf(perm: Seq[Int]): Cycles = cyclesOf(perm, Set())
-  def cyclesOf(perm: Seq[Int], ready: Set[Int]): Cycles =
-    (1 to perm.length) find (i => !ready.contains(i)) match { // bis zu n Aufrufe; Zugriff auf Hashset O(1)
+  def cyclesOf(perm: Seq[Int], handled: Set[Int]): Cycles =
+    (1 to perm.length) find (i => !handled.contains(i)) match { // bis zu n Aufrufe; Zugriff auf Hashset O(1)
       case Some(start) =>
         val newCycle = cycle(perm, start)
-        val newReady = ready ++ newCycle // O(n)
+        val newReady = handled ++ newCycle // O(n)
         newCycle :: cyclesOf(perm, newReady)
       case None =>
         Nil
@@ -37,26 +37,28 @@ object SlowCycler extends Cycler {
 
 object FastCycler extends Cycler {
   /** Return the list of disjunct cycles sorted ascending by cycle.head */
-  def cyclesOf(perm: Seq[Int]): Cycles = cyclesOf(Nil, perm, new Array[Boolean](perm.length))
+  def cyclesOf(perm: Seq[Int]): Cycles =
+    cyclesOf(Nil, perm, new Array[Boolean](perm.length), 0)
 
-  @tailrec private def cyclesOf(ready: List[Cycle], perm: Seq[Int],
-                                handled: Array[Boolean], start: Int = 1): Cycles = {// c *
-    val aCycle = cycle(perm, start) // O(n_c)
-    for (i <- aCycle) { handled(i-1) = true } // O(n_c); Side effects are not harmful, it's tailrec!
-    (start to perm.length) find (i => !(handled(i-1))) match { // O(i_c) //\sum{i_c} = n
-      case Some(next) =>
-        cyclesOf(aCycle :: ready, perm, handled, next)
+  @tailrec private
+  def cyclesOf(ready: List[Cycle], perm: Seq[Int],
+               handled: Array[Boolean], prev: Int): Cycles = // c *
+    (prev+1 to perm.length) find (i => !(handled(i-1))) match { // O(i_c) //\sum{i_c} = n
+      case Some(start) =>
+        val aCycle = cycle(perm, start)
+        for (i <- aCycle)
+          handled(i-1) = true // O(n_c); Side effects are not harmful, it's tailrec!
+        cyclesOf(aCycle :: ready, perm, handled, start)
       case None =>
-        (aCycle :: ready).reverse // O(1)
+        ready.reverse // O(n)
     }
-  }
   /** Small helper function, finding one cycle. */
   private def cycle(perm: Seq[Int], start: Int): Cycle = {// O(n_c)
-    @tailrec def step(ready: ListBuffer[Int], idx: Int): Cycle = // O(n_c)
+    @tailrec def step(ready: List[Int], idx: Int): Cycle = // O(n_c)
       if(start == idx)
-        ready.toList // O(1)
+        ready.reverse // O(1)
       else
-        step(ready += idx,perm(idx - 1))
-    (start :: step(new ListBuffer[Int], perm(start - 1)))
+        step(idx :: ready, perm(idx - 1))
+    (start :: step(Nil, perm(start - 1)))
   }
 }
