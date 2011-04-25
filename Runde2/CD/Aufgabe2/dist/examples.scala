450fce8dcccdd3192807143fd8acb644bb32c9e8
Tim Dev <tim8dev@gmail.com>
2011-04-25 13:45:40 +0200

    Adding Aufgabe./Kisten.jar!

---
+++ Runde2/CD/Aufgabe2/dist/examples.scala
@@ -0,0 +1,23 @@
+println("# Die Beispiele aus der Aufgabenstellung folgen nun.")
+println("# Die zeitaufwändingen Beispiele wurden ausgelassen.")
+
+println("## 4.1 Beispiel aus der Aufgabenstellung: ")
+
+val perm = Seq(4,3,2,1)
+
+val verified = Utils demonstrate perm
+
+println("## 4.1 Ende")
+
+println("## 4.2 Spezialfälle")
+
+println("### 4.2.1 Äquivalenzklassen")
+
+val perm = Seq(2,1,4,3)
+
+val verified = Utils demonstrate perm
+
+println("### 4.2.2 Ende ")
+
+println("## 4.2 Ende ")
+
---
+++ Runde2/CD/run.scala
@@ -0,0 +1,11 @@
+import de.voodle.tim.bwinf.{kisten, container}
+import kisten._, Kiste._, StandardKisten._
+import container._
+
+println("29. Bundeswettbewerb Informatik 2010/2011")
+println(" 2. Runde ")
+println("Einsendung: ")
+println("  Von: Tim Taubner")
+println("  Verwaltungsnummer: 29.0108.01")
+println("  Erste bearbeitete Aufgabe:  (1) Kisten in Kisten in Kisten")
+println("  Zweite bearbeitete Aufgabe: (2) Containerklamüsel")
